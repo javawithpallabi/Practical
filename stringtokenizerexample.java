4249a22eda161cb9c60500bd2c2974992485d629
javawithpallabi <104891926+javawithpallabi@users.noreply.github.com>
2022-05-06 11:38:17 +0530

    Add files via upload

---
+++ stringtokenizerexample.java
@@ -0,0 +1,23 @@
+import java.util.StringTokenizer;
+public class stringtokenizerexample {
+	public static void my() {
+		StringTokenizer t = new StringTokenizer("Anudip Foundation skill and Carrer Development Center", " ");
+	while(t.hasMoreTokens()) {
+		System.out.println(t.nextToken(" "));{
+			StringTokenizer st = new StringTokenizer("I am riya, Staying in Kolkata");
+	while(st.hasMoreTokens()) {
+		System.out.println(st.nextToken(","));
+		}
+			
+		}
+		
+	}
+	}
+
+	public static void main(String[] args) {
+		my();
+		// TODO Auto-generated method stub
+
+	}
+
+}
