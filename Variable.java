ee7871a916b1e04eff1a270c08f59f4db6f26a1e
javawithpallabi <104891926+javawithpallabi@users.noreply.github.com>
2022-05-18 12:29:03 +0530

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,12 @@
+class VariableExample {
+public void display() {
+int num1 = 100;
+int num2 = 400;
+int res = num1+num2;
+System.out.println("the addition is : "+ res);
+}
+public static void main (String[] args) {
+VariableExample a = new VariableExample();
+a.display();
+}
+}
