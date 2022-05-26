8b99efb80c13566e69b8473e8cab9402bdeedd28
javawithpallabi <104891926+javawithpallabi@users.noreply.github.com>
2022-05-26 13:45:23 +0530

    Create Inherit.java

---
+++ Inherit.java
@@ -0,0 +1,16 @@
+class Metal {
+String weight ="100kg";
+String color ="Golden";
+}
+class Gold extends Metal {
+String hue ="Hue";
+}
+class sampleinherit {
+public static void main(String[]args) {
+Gold g = new Gold();
+System.out.println("Metal weight is :" + g.weight);
+System.out.println("Metal color is :" + g.color);
+}
+}
+© 2022 GitHub, Inc.
+Terms
