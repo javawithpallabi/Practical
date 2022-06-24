279d9396b7dfa347a4421eb504923de0e3ab2cfa
javawithpallabi <104891926+javawithpallabi@users.noreply.github.com>
2022-06-24 10:52:44 +0530

    Update ArrayExample.java

--- ArrayExample.java
+++ ArrayExample.java
@@ -2,7 +2,7 @@ class ArrayExample {
 public static void main (String args[]) {
 String [] names = {"Mamta","Ram","Karan","Ajay","Ajith","Tushar"};
 for (int i=0;i<names.length;i++) {
-System.out.println(names[i]);
+System.out.println(names[i]); // used for print satement
 }
 System.out.println();
 int [] phone = {123,456,567,789,955,333,555,111,2345,7890};
