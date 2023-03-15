fb20d3acd294cf6c813367edc13963b9b2eaf7b1
javawithpallabi <104891926+javawithpallabi@users.noreply.github.com>
2023-03-15 13:25:14 +0530

    Add files via upload

--- A.java
+++ A.java
@@ -1,13 +1,13 @@
 class A
 {
 
-static int c = 30; 
-int a = 10 ;
+static int c = 30; // Sv // with the help of classname dot variable name
+int a = 10 ; // Iv // obj create object
 public static void main(String ...args)
 {
-A obj = new A();
+A obj = new A();// creating object
 
-int b=20 ;
+int b=20 ;// Lc // directly
 
  System.out.println(A.c);
  System.out.println(b);
